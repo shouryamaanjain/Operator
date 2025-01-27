9b09c9e7b6e085191a1bdb27826811dd2411e6bb
Shouryamaan Jain <jainshouryamaan@gmail.com>
2025-01-27 17:22:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { createClient } from '@/utils/supabase/server'
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 import { AnonShield } from '@/components/anon-shield'
-import PostHogClient from '@/utils/posthog-server'
 
 export default async function IndexPage() {
   // check if there is a session - if not, render the Anon Shield
@@ -11,18 +10,6 @@ export default async function IndexPage() {
   const {
     data: { user }
   } = await supabase.auth.getUser()
-  const posthog = PostHogClient()
-  if (!user) {
-    posthog.capture({
-      distinctId: 'anon',
-      event: 'page_view'
-    })
-  } else {
-    posthog.capture({
-      distinctId: user.id,
-      event: 'page_view'
-    })
-  }
 
   const id = nanoid()
 
@@ -33,3 +20,4 @@ export default async function IndexPage() {
     </>
   )
 }
+
